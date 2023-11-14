fc7c4552d3cb5ce585d800f02e4e2454e017c676
luan <luan640@gmail.com>
2023-11-14 11:50:32 -0300

    versao com api

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from openpyxl.styles import Font
 from openpyxl import load_workbook
 from openpyxl.writer.excel import save_virtual_workbook
 
+import requests
+from PIL import Image
+from io import BytesIO, StringIO
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image as ExcelImage
+
 st.title("Formatar arquivos RNCAC")
 
 file_upload = st.file_uploader("Anexe o arquivo original baixado do monday", type='xlsx')
@@ -11,7 +17,8 @@ file_upload = st.file_uploader("Anexe o arquivo original baixado do monday", typ
 if file_upload is not None:
     # Salva o arquivo temporário
     temp_file = 'arquivo_formatado.xlsx'
-    
+    # temp_file = r"C:\Users\pcp2\Downloads\RNCAC_CQ_1698677290 (3).xlsx"
+
     with open(temp_file, 'wb') as file:
         file.write(file_upload.getvalue())
 
@@ -19,73 +26,101 @@ if file_upload is not None:
 
     excelOriginal = pd.read_excel(temp_file)
 
-    id = excelOriginal['Unnamed: 2'][4]
-    item = excelOriginal['RNCAC'][4]
-    log_criacao = excelOriginal['Unnamed: 3'][4]
-    origem = excelOriginal['Unnamed: 4'][4]
-    setor = excelOriginal['Unnamed: 5'][4]
-    conjunto = excelOriginal['Unnamed: 6'][4]
-    responsavel = excelOriginal['Unnamed: 9'][4]
-    gravidade = excelOriginal['Unnamed: 10'][4]
-    prioridade = excelOriginal['Unnamed: 11'][4]
-    status = excelOriginal['Unnamed: 12'][4]
-    ultima_atualizacao = excelOriginal['Unnamed: 16'][4]
+    id = excelOriginal['Unnamed: 2'][4] #B7
+    # item = excelOriginal['RNCAC'][4] 
+    # log_criacao = excelOriginal['Unnamed: 3'][4]
+    # origem = excelOriginal['Unnamed: 4'][4]
+    setor = excelOriginal['Unnamed: 5'][4] #D7
+    # conjunto = excelOriginal['Unnamed: 6'][4] 
+    responsavel = excelOriginal['Unnamed: 9'][4] #G8 G15
+    # gravidade = excelOriginal['Unnamed: 10'][4]
+    # prioridade = excelOriginal['Unnamed: 11'][4]
+    # status = excelOriginal['Unnamed: 12'][4] 
+    # ultima_atualizacao = excelOriginal['Unnamed: 16'][4]
 
-    descricao = excelOriginal['Unnamed: 7'][4]
-    arquivos = excelOriginal['Unnamed: 8'][4]
-    conclusao = excelOriginal['Unnamed: 14'][4]
-    avaliacao = excelOriginal['Unnamed: 13'][4]
-    
+    descricao = excelOriginal['Unnamed: 7'][4] #A11
+    arquivos = excelOriginal['Unnamed: 7'][6]
+    arquivos_split = arquivos.split()
+    foto = arquivos_split[0].replace(",","")
+
+    import requests
+
+    img_data = requests.get(foto).content
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(img_data)
+
+
+    # conclusao = excelOriginal['Unnamed: 14'][4]
+    acao_contencao = excelOriginal['Unnamed: 13'][4] #A17
+
+
+    mao_de_obra = excelOriginal['Unnamed: 17'][4] #B25
+    materia_prima = excelOriginal['Unnamed: 18'][4] #B26
+    medicao = excelOriginal['Unnamed: 19'][4] #B27
+    metodo = excelOriginal['Unnamed: 20'][4] #B28
+    meio_ambiente = excelOriginal['Unnamed: 21'][4] #B29
+
+    participantes = excelOriginal['Unnamed: 23'][4] #C30
+
     excelOriginal_cortado = excelOriginal.iloc[5:].reset_index(drop=True)
     # excelOriginal_cortado = excelOriginal_cortado.set_axis(excelOriginal_cortado.iloc[0], axis='columns', inplace=False)
     excelOriginal_cortado = excelOriginal_cortado.set_axis(excelOriginal_cortado.iloc[0], axis='columns')
     excelOriginal_cortado = excelOriginal_cortado[1:].reset_index(drop=True)
+    
+    print(excelOriginal_cortado)
 
-    wb = load_workbook('modeloFinal.xlsx')
+
+    wb = load_workbook('modelo_final_v2.xlsx')
     ws = wb.active
 
     ws['B7'] = id
-    ws['B8'] = item
-    ws['B9'] = log_criacao
-    ws['B10'] = origem
-    ws['B11'] = setor
-    ws['B12'] = conjunto
-    ws['B13'] = responsavel
-    ws['B14'] = gravidade
-    ws['B14'].font = font_bold
+    ws['D7'] = setor
+    ws['G8'] = responsavel
+    ws['G14'] = responsavel
+    ws['A10'] = descricao
+    ws['A16'] = acao_contencao
+    ws['B24'] = mao_de_obra
+    ws['B25'] = materia_prima
+    ws['B26'] = medicao
+    ws['B27'] = metodo
+    ws['B28'] = meio_ambiente
+    ws['C29'] = participantes
 
-    ws['B15'] = prioridade
-    ws['B15'].font = font_bold
+    # Adiciona a imagem à planilha do Excel
+    excel_img = ExcelImage(img_data)
+    ws.add_image(excel_img, 'A1')
 
-    ws['B16'] = status
-    ws['B16'].font = font_bold
+    # ws['B14'].font = font_bold
 
-    ws['A20'] = descricao
-    ws['A18'] = arquivos
-    ws['D45'] = ultima_atualizacao
+    # ws['B15'] = prioridade
+    # ws['B15'].font = font_bold
 
-    ws['A42'] = conclusao
-    ws['D44'] = avaliacao
+    # ws['B16'] = status
+    # ws['B16'].font = font_bold
 
-    excelOriginal_cortado['Prazo'] = pd.to_datetime(excelOriginal_cortado['Prazo'])
-    excelOriginal_cortado['Prazo'] = excelOriginal_cortado['Prazo'].dt.strftime('%d-%m-%Y')
+    # ws['A20'] = descricao
+    # ws['A18'] = arquivos
+    # ws['D45'] = ultima_atualizacao
+
+    # ws['A42'] = conclusao
+    # ws['D44'] = avaliacao
+
+    excelOriginal_cortado['Previsão'] = pd.to_datetime(excelOriginal_cortado['Previsão'])
+    excelOriginal_cortado['Previsão'] = excelOriginal_cortado['Previsão'].dt.strftime('%d-%m-%Y')
     excelOriginal_cortado = excelOriginal_cortado.fillna('')
 
-    ultimaLinha = len(excelOriginal_cortado) + 23 - 2
-    u = 23
+    u = 34
+    ultimaLinha = len(excelOriginal_cortado) + u-1
 
-    df_status = excelOriginal_cortado.iloc[:,6:7]
+    df_status = excelOriginal_cortado.iloc[:,5:6]
 
     try:
 
         while u < ultimaLinha:
             for i in range(len(excelOriginal_cortado)):
                 ws['A' + str(u)] = excelOriginal_cortado['Name'][i]
-                ws['B' + str(u)] = excelOriginal_cortado['Ação Imediata'][i]
-                ws['C' + str(u)] = excelOriginal_cortado['Prazo'][i]
-                ws['D' + str(u)] = excelOriginal_cortado['Responsável'][i]
-                ws['E' + str(u)] = df_status['Status'][i]
-                ws['F' + str(u)] = excelOriginal_cortado['Avaliação'][i]
+                ws['F' + str(u)] = excelOriginal_cortado['Previsão'][i]
+                ws['E' + str(u)] = excelOriginal_cortado['Responsável'][i]
                 u += 1
     except:
         pass
@@ -101,5 +136,9 @@ if file_upload is not None:
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
 
+    # Remove o arquivo temporário
+    import os
+    os.remove(img_path)
+    
     # Exibe o botão de download
     st.write(download_button)
